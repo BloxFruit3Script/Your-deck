50a6e1253793989ce062ad5f060b27643dc85006
BloxFruit3Script <168387835+BloxFruit3Script@users.noreply.github.com>
2024-09-03 12:10:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -121,38 +121,6 @@ def bypass():
     else:
         return jsonify({"message": "Please Enter a Valid Fluxus Link!"}), 400
         
-@app.route('/api/boost.ink', methods=['GET'])
-def extract():
-    # Get the URL from query parameters
-    url = request.args.get('url')
-    
-    if not url:
-        return jsonify({"error": "URL is required"}), 400
-    
-    try:
-        # Fetch the webpage content
-        response = requests.get(url)
-        html_content = response.text
-
-        # Regex pattern to match the Base64 value inside bufpsvdhmjybvgfncqfa attribute
-        pattern = r'bufpsvdhmjybvgfncqfa="([^"]+)"'
-
-        # Find all matches
-        matches = re.findall(pattern, html_content)
-
-        if matches:
-            # Extract the first match
-            base64_value = matches[0]
-
-            # Decode the Base64 value
-            decoded_value = base64.b64decode(base64_value).decode("utf-8")
-            
-            return jsonify({"bypassed": decoded_value})
-        else:
-            return jsonify({"error": "No matches found"}), 404
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
 if __name__ == '__main__':
     app.run(
         host='0.0.0.0',
